3eaa1f99eea19a2ca06607ffb6c69b97cda3b97d
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-11-26 11:11:00 -0500

    Update index.php

--- index.php
+++ index.php
@@ -10,8 +10,11 @@ $htmlContent = '<!DOCTYPE html>
 </body>
 </html>';
 
+// Specify the directory where you want to save the file
+$directory = '/GeneratedFiles/';
+
 // Generate a unique file name based on timestamp and random string
-$uniqueFileName = 'generated_' . time() . '_' . bin2hex(random_bytes(4)) . '.html';
+$uniqueFileName = $directory . 'generated_' . time() . '_' . bin2hex(random_bytes(4)) . '.html';
 
 // Open the file for writing
 $file = fopen($uniqueFileName, 'w');
@@ -23,9 +26,9 @@ fwrite($file, $htmlContent);
 fclose($file);
 
 // Commit and push the changes to GitHub
-exec('git add ' . $fileName);
-exec('git commit -m "Add generated HTML file"');
+exec('git add ' . $uniqueFileName);
+exec('git commit -m "Add generated HTML file" ' . $uniqueFileName);
 exec('git push origin master');
 
-echo "HTML file '$uniqueFileName' has been created successfully.";
+echo "HTML file '$uniqueFileName' has been created and committed successfully.";
 ?>
