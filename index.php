f09214fc2ec8e784e78397150d1deee4c16b482e
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-11-25 20:14:32 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+// Set the content for the HTML file
+$htmlContent = '<!DOCTYPE html>
+<html>
+<head>
+    <title>Generated HTML</title>
+</head>
+<body>
+    <p>This is a paragraph of text in the generated HTML file.</p>
+</body>
+</html>';
+
+// Generate a unique file name based on timestamp and random string
+$uniqueFileName = 'generated_' . time() . '_' . bin2hex(random_bytes(4)) . '.html';
+
+// Open the file for writing
+$file = fopen($uniqueFileName, 'w');
+
+// Write the HTML content to the file
+fwrite($file, $htmlContent);
+
+// Close the file
+fclose($file);
+
+echo "HTML file '$uniqueFileName' has been created successfully.";
+?>
