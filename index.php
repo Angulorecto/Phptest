1541b1253803dc55ecfc21cb05a382f4dcd7e893
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-11-26 11:08:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,31 @@
 <?php
-$directory = '/GeneratedFiles/';
-$filename = 'generated_' + time() + rand(1,1000)+'.html';
-$filecontent = 'Hello, world!';
+// Set the content for the HTML file
+$htmlContent = '<!DOCTYPE html>
+<html>
+<head>
+    <title>Generated HTML</title>
+</head>
+<body>
+    <p>This is a paragraph of text in the generated HTML file.</p>
+</body>
+</html>';
 
-// Make sure the directory exists
-if (!file_exists($directory)) {
-    mkdir($directory, 0777, true);
-}
+// Generate a unique file name based on timestamp and random string
+$uniqueFileName = 'generated_' . time() . '_' . bin2hex(random_bytes(4)) . '.html';
 
 // Open the file for writing
-$file = fopen($directory . $filename, 'w');
+$file = fopen($uniqueFileName, 'w');
 
-// Write the content to the file
-fwrite($file, $filecontent);
+// Write the HTML content to the file
+fwrite($file, $htmlContent);
 
 // Close the file
 fclose($file);
 
-echo 'File ' + $filename + ' created successfully!';
+// Commit and push the changes to GitHub
+exec('git add ' . $fileName);
+exec('git commit -m "Add generated HTML file"');
+exec('git push origin master');
+
+echo "HTML file '$uniqueFileName' has been created successfully.";
 ?>
